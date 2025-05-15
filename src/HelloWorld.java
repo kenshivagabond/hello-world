a86e350ba1c10c08037cdf37efde0f928977b01e
FILLONNEAU Kenzo <12401880@r20208.iutv.univ-paris13.fr>
2025-05-15 15:02:15 +0200

    nv code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,8 @@
-
-public class HelloWord {
-
+public class HelloWorld {
+ 
 	public static void main(String[] args) {
 		System.out.println("Hello World !");
+		System.out.println("Hola Mundo !");
 	}
-
+ 
 }
